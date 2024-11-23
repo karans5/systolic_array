033deefbc06852a1c6c791ad33c0776f54621d73
malvika1606 <malavikamalusanjeev@gmail.com>
2024-11-24 00:05:40 +0530

    Update test_systolic_array.py

--- src/test_systolic_array.py
+++ src/test_systolic_array.py
@@ -9,3 +9,22 @@ import struct
 
 #import coverage
 from model_systolic_array import *
+
+# Inputs
+
+@cocotb.test()
+async def test_mac(dut):
+    '''Test the ARRAY module'''
+
+    # Initialize and start the clock
+    clock = Clock(dut.CLK, 10, units="us")
+    cocotb.start_soon(clock.start(start_high=False))
+
+    # Reset the DUT
+    dut.RST_N.value = 0
+    await RisingEdge(dut.CLK)
+    dut.RST_N.value = 1
+    await RisingEdge(dut.CLK)
+
+
+   
